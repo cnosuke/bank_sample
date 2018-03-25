1594d1e4b7d1978271296cdc15aef4a326fe6bde
cnosuke <shinnosuke@gmail.com>
2018-03-25 16:34:45 +0900

    Update sample_client/client

--- sample_client/client.rb
+++ sample_client/client.rb
@@ -1,5 +1,4 @@
 require 'grpc'
-require 'pry'
 require 'active_support/all'
 
 proto_lib_dir = "#{__dir__}/../proto/lib/"
@@ -10,6 +9,14 @@ PORT = '127.0.0.1:8080'
 INSECURE = :this_channel_is_insecure
 
 svc = Bank::WalletService::Stub.new(PORT, INSECURE)
+
+# GetByUserID sample
+req = Bank::GetByUserIDRequest.new(user_id: 1)
+
+res = svc.get_by_user_id(req)
+p res.wallet
+
+# AddPaymentsRequest sample
 payments = [
   Bank::Payment.new(amount: -100, target: '自販機', reason: 'コーラ'),
   Bank::Payment.new(amount: -500, target: 'コンビニ', reason: 'おにぎり'),
@@ -18,5 +25,3 @@ req = Bank::AddPaymentsRequest.new(user_id: 1, payments: payments)
 
 res = svc.add_payments(req)
 p res.wallet
-
-binding.pry
